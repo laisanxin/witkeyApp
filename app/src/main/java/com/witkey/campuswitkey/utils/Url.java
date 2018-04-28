be9a0dcbadef0d3b957038f248368b744b4e5e72
laisanxin <1255490926@qq.com>
2018-04-28 15:12:03 +0800

    test

--- app/src/main/java/com/witkey/campuswitkey/utils/Url.java
+++ app/src/main/java/com/witkey/campuswitkey/utils/Url.java
@@ -8,5 +8,5 @@ public class Url {
     public static final String Base_URL = "http://10.0.2.2:8080/witkeyServer/";
     /*public static final String Base_URL = "http://tc.lsxweb.top/witkey/";*/
     /*public static final String Base_URL = "http://tc.lsxweb.top/witkey/";*/
- /*public static final String Base_URL = "http://tc.lsxweb.top/witkey/";*/
+    /*public static final String Base_URL = "http://tc.lsxweb.top/witkey/";*/
 }
