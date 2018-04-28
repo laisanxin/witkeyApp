d0bc670518ae823971da045900994f6888d40270
laisanxin <1255490926@qq.com>
2018-04-28 15:42:53 +0800

    test

--- app/src/main/java/com/witkey/campuswitkey/views/MainActivity.java
+++ app/src/main/java/com/witkey/campuswitkey/views/MainActivity.java
@@ -2,10 +2,19 @@ package com.witkey.campuswitkey.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.witkey.campuswitkey.R;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.find_tasks_tv) TextView find_tasks_tv;
+    @BindView(R.id.find_witkeys_tv) TextView find_witkeys_tv;
+    @BindView(R.id.post_tasks_tv) TextView post_tasks_tv;
+    @BindView(R.id.personal_tv) TextView personal_tv;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
